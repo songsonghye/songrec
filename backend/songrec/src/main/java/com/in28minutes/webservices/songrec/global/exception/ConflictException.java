ab6ae6049a4972189a4d12d4f6fb06290ac8fc69
hyewon <khw3480000@naver.com>
2025-12-14 22:18:51 +0900

    [refactor] Request API DTO 적용 및 user scope 검증 정리
    - RequestController DTO 누락 부분 수정
    - Request API 엔드포인트를 user 기준 스코프로 통일
    - userId 기반 요청 접근 검증 로직 추가
    - RequestService 조회/수정 메서드 구조 정리

---
+++ backend/songrec/src/main/java/com/in28minutes/webservices/songrec/global/exception/ConflictException.java
@@ -0,0 +1,7 @@
+package com.in28minutes.webservices.songrec.global.exception;
+
+public class ConflictException extends RuntimeException {
+    public ConflictException(String message) {
+        super(message);
+    }
+}
